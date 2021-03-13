a38c3a5b76c30c8a701c95d684e22be41bb0b6c6
Shahzab Zaman <o.aszaman00@gmail.com>
2021-03-13 00:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  console.log(string.toUpperCase())
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else {
+    return "I love you, too."
+  }
 }
